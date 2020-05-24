c045922dd7b5b5e9b9e9490b65944605bf991706
Natay Aberra <natay.aberra@gmail.com>
2020-05-24 16:12:15 -0400

    moving scripts over from biostar-recipes

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Bioinformatics Recipes
+"""
+from setuptools import setup
+
+VERSION = "1.0"
+setup(
+    name="Bioinformatics Recipes",
+    version=VERSION,
+    packages=[
+        "src",
+    ],
+)
+

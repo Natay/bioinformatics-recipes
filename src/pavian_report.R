e5908a5d90df6d01d6f25936be056ff52954d10c
Natay Aberra <natay.aberra@gmail.com>
2020-05-25 17:42:50 -0400

    working on pavian

--- src/pavian_report.R
+++ src/pavian_report.R
@@ -12,23 +12,24 @@ args = commandArgs(trailingOnly=TRUE)
 # Directory containing input data files.
 INPUT_DIR = args[1]
 
-# Directory to store the final HTML report.
-OUTPUT_DIR = args[2]
-
-# Ensure DIRs does not have a trailing /
-
-# Output file name
-OUT_FILE = args[3] #'pavion-report.html'
-
-# Report title
-REPORT_TITLE = args[4] #"Report title"
-
 # Date to put on the report
-DATE = args[5]
+DATE = args[2]
 
 # Comma seperated string to filter taxa in sankey.
 # Example: Chordata,artificial sequences
-FILTER_TAXA = trimws(unlist(strsplit(args[6], ",")))
+FILTER_TAXA = trimws(unlist(strsplit(args[3], ",")))
+
+# Comma seperated string of
+SANKEY_RANKS = args[4]
+
+# Get current working directory as output
+OUTDIR = getwd()
+
+# Output file name
+OUT_FILE = 'pavion-report.html'
+
+# Report title
+REPORT_TITLE = "Pavian classification report "
 
 # Combine sample names into a single
 sample_set_names_combined <- function(){
@@ -46,16 +47,23 @@ sample_set_names_combined <- function(){
 #data <- pavian::read_sample_data(system.file("shinyapp","example-data","brain-biopsies",package="pavian"))
 data <- pavian::read_sample_data(dir(INPUT_DIR, pattern=NULL, all.files=FALSE, full.names=TRUE), is_files=TRUE)
 
+
+template = file.path(OUTDIR, 'pavian_template.Rmd')
+
+if file.exists(template){
+    temp <- template
+}else{
+    # Get the R Markdown template for final report file
+    rmd_file <- system.file("pavian-report.Rmd",package="pavian")
+
+    # Get the R Markdown template file locally or from the
+    # Make a local copy of the template file
+    temp <- file.path(OUTPUT_DIR, 'pavian_template.Rmd' )
+    file.copy(rmd_file, local_temp, overwrite = TRUE)
+}
+
 # Gather the reports
 reports <- pavian::read_reports(data$ReportFilePath, data$Name)
-
-# Get the R Markdown template for final report file
-rmd_file <- system.file("pavian-report.Rmd",package="pavian")
-
-# Make a local copy of the template file
-local_temp <- file.path(OUTPUT_DIR, "template.Rmd")
-
-file.copy(rmd_file, local_temp, overwrite = TRUE)
 
 # Set up parameters to pass to Rmd document
 params <- list(doc_title=REPORT_TITLE,
@@ -66,10 +74,11 @@ params <- list(doc_title=REPORT_TITLE,
              sample_data=data,
              reports=reports,
              include_sankey=TRUE,
-             filter_taxa=FILTER_TAXA)
+             filter_taxa=FILTER_TAXA,
+             tax_rank=SANKEY_RANKS)
 
 # Create the final html report.
-rmarkdown::render(local_temp, output_file = OUT_FILE ,
+rmarkdown::render(temp, output_file = OUT_FILE ,
                   params = params, output_format = "html_document",
                   envir = new.env())
 

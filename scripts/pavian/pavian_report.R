bc16f4e502ea1881b84790ce42abead6588f84b7
Natay Aberra <natay.aberra@gmail.com>
2020-06-04 12:24:40 -0400

    output file name added as parameter

--- scripts/pavian/pavian_report.R
+++ scripts/pavian/pavian_report.R
@@ -21,7 +21,7 @@ SANKEY_RANKS = args[4]
 OUTDIR = getwd()
 
 # Output file name
-OUT_FILE = 'pavion-report.html'
+OUT_FILE = args[5] | "pavian-report.html"
 
 # Report title
 REPORT_TITLE = "Pavian classification report "
